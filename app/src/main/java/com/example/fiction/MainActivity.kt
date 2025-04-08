ca0de07d9c6d06b9150e61143151c29109fd3cbd
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-08 10:44:09 +0300

    feat: initialize binding and onCreate in MainActivity

--- app/src/main/java/com/example/fiction/MainActivity.kt
+++ app/src/main/java/com/example/fiction/MainActivity.kt
@@ -1,5 +1,16 @@
 package com.example.fiction
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fiction.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {}
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+    }
+}
