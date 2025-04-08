ccc6e7d3b9c1ea56950705058c4499f5b59380ee
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-08 15:49:07 +0300

    feat: add binding and onCreateView in LibraryFragment

--- app/src/main/java/com/example/fiction/fragments/LibraryFragment.kt
+++ app/src/main/java/com/example/fiction/fragments/LibraryFragment.kt
@@ -1,6 +1,21 @@
 package com.example.fiction.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fiction.databinding.FragmentLibraryBinding
 
 class LibraryFragment : Fragment() {
+
+    private lateinit var libraryBinding: FragmentLibraryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        libraryBinding = FragmentLibraryBinding.inflate(layoutInflater)
+        return libraryBinding.root
+    }
 }
