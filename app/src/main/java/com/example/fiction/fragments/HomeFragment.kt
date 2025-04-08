89318a35521ba3823b2c80d686c6e0f6c75393ca
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-08 15:43:16 +0300

    feat: add binding and onCreateView in HomeFragment

--- app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
@@ -1,6 +1,21 @@
 package com.example.fiction.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fiction.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var homeBinding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        homeBinding = FragmentHomeBinding.inflate(layoutInflater)
+        return homeBinding.root
+    }
 }
