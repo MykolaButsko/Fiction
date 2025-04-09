bf0be83785d4145648bf4d97243e81ccf5501574
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-09 13:05:36 +0300

    feat: add data class BookDescription

---
+++ app/src/main/java/com/example/fiction/dataClasses/BookDescription.kt
@@ -0,0 +1,9 @@
+package com.example.fiction.dataClasses
+
+data class BookDescription(
+    val img: Int,
+    val bookName: String,
+    val bookAuthor: String,
+    val aboutBook: String,
+    val bookDescription: String
+)
