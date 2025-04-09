ddd4b51264bea30b3963af67479bf15080528fb5
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-09 19:40:32 +0300

    feat: implement getBooks() in BookRepository

--- app/src/main/java/com/example/fiction/viewmodel/BookRepository.kt
+++ app/src/main/java/com/example/fiction/viewmodel/BookRepository.kt
@@ -1,4 +1,52 @@
 package com.example.fiction.viewmodel
 
-class BookRepository {
+import android.content.Context
+import com.example.fiction.R
+import com.example.fiction.dataClasses.Book
+
+class BookRepository(private val context: Context) {
+
+    private val booksList: MutableList<Book> = mutableListOf()
+
+    fun getBooks(): List<Book> {
+        if (booksList.isEmpty()) {
+            booksList.addAll(
+                listOf(
+                    Book(
+                        1,
+                        context.getString(R.string.app_name),
+                        R.drawable.img_1,
+                        R.drawable.selector_like,
+                        context.getString(R.string.name_1),
+                        context.getString(R.string.author_1)
+                    ),
+                    Book(
+                        2,
+                        context.getString(R.string.culture_and_society),
+                        R.drawable.img_2,
+                        R.drawable.selector_like,
+                        context.getString(R.string.name_2),
+                        context.getString(R.string.author_2)
+                    ),
+                    Book(
+                        3,
+                        context.getString(R.string.mind_and_philosophy),
+                        R.drawable.img_3,
+                        R.drawable.selector_like,
+                        context.getString(R.string.name_3),
+                        context.getString(R.string.author_3)
+                    ),
+                    Book(
+                        4,
+                        context.getString(R.string.personal_growth),
+                        R.drawable.img_4,
+                        R.drawable.selector_like,
+                        context.getString(R.string.name_4),
+                        context.getString(R.string.author_4)
+                    )
+                )
+            )
+        }
+        return booksList
+    }
 }
