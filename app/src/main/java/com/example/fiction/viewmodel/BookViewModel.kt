0078fe79c2465e80e9599fe3265399bbccb98bf9
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-09 21:09:45 +0300

    feat: add loadBook() logic to BookViewModel

--- app/src/main/java/com/example/fiction/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/example/fiction/viewmodel/BookViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.fiction.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.fiction.dataClasses.Book
 
-class BookViewModel : ViewModel() {
+class BookViewModel(private val bookRepository: BookRepository) : ViewModel() {
+
+    private val _bookList = MutableLiveData<List<Book>>()
+    val bookListLiveData: LiveData<List<Book>> = _bookList
+
+    fun loadBook() {
+        _bookList.value = bookRepository.getBooks()
+    }
 }
