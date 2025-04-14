bb7ebbc1011d6615b1e590135d2e1b789823a941
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-14 10:32:45 +0300

    chore: add @Suppress to BookViewModelFactory

--- app/src/main/java/com/example/fiction/viewmodel/BookViewModelFactory.kt
+++ app/src/main/java/com/example/fiction/viewmodel/BookViewModelFactory.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+@Suppress("UNCHECKED_CAST")
 class BookViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
