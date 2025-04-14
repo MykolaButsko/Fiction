dd1a8a0798bb9a052e1b05e198db6d30d5e1c5c6
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-14 17:29:30 +0300

    feat: add chooseGenreBook() to HomeFragment

--- app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.fiction.BookAdapter
+import com.example.fiction.R
 import com.example.fiction.databinding.FragmentHomeBinding
 import com.example.fiction.viewmodel.BookViewModel
 import com.example.fiction.viewmodel.BookViewModelFactory
@@ -29,6 +30,7 @@ class HomeFragment : Fragment() {
         homeBinding = FragmentHomeBinding.inflate(layoutInflater)
 
         init()
+        chooseGenreBook()
 
         return homeBinding.root
     }
@@ -47,6 +49,28 @@ class HomeFragment : Fragment() {
     private fun observeAndUpdateBooks() {
         bookViewModel.bookListLiveData.observe(viewLifecycleOwner) { books ->
             bookAdapter.submitList(books)
+        }
+    }
+
+    private fun chooseGenreBook() = with(homeBinding) {
+        buttonFiction.setOnClickListener {
+            bookViewModel.loadBook()
+            textViewFiction.text = (getString(R.string.app_name))
+        }
+
+        buttonCultureAndSociety.setOnClickListener {
+            bookViewModel.filterBooksByGenre(getString(R.string.culture_and_society))
+            textViewFiction.text = (getString(R.string.culture_and_society))
+        }
+
+        buttonMindAndPhilosophy.setOnClickListener {
+            bookViewModel.filterBooksByGenre(getString(R.string.mind_and_philosophy))
+            textViewFiction.text = (getString(R.string.mind_and_philosophy))
+        }
+
+        buttonPersonalGrowth.setOnClickListener {
+            bookViewModel.filterBooksByGenre(getString(R.string.personal_growth))
+            textViewFiction.text = (getString(R.string.personal_growth))
         }
     }
 }
