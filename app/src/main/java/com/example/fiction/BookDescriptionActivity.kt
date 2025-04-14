872b6d2f1765d6822e425eb67cad872cca277412
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-14 21:40:37 +0300

    feat: add binding and onCreate in BookDescriptionActivity

--- app/src/main/java/com/example/fiction/BookDescriptionActivity.kt
+++ app/src/main/java/com/example/fiction/BookDescriptionActivity.kt
@@ -1,6 +1,16 @@
 package com.example.fiction
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.fiction.databinding.ActivityBookDescriptionBinding
 
 class BookDescriptionActivity : AppCompatActivity() {
+
+    private lateinit var activityBookDescription: ActivityBookDescriptionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityBookDescription = ActivityBookDescriptionBinding.inflate(layoutInflater)
+        setContentView(activityBookDescription.root)
+    }
 }
