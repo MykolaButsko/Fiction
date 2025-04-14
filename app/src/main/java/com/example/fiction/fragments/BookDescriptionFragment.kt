e002854a8e1fb1fa9d63a552fd57f9aa9f706416
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-14 21:54:30 +0300

    feat: add binding and onCreateView in BookDescriptionFragment

--- app/src/main/java/com/example/fiction/fragments/BookDescriptionFragment.kt
+++ app/src/main/java/com/example/fiction/fragments/BookDescriptionFragment.kt
@@ -1,6 +1,22 @@
 package com.example.fiction.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fiction.databinding.FragmentBookDescriptionBinding
 
 class BookDescriptionFragment : Fragment() {
+
+    private lateinit var bookDescriptionBinding: FragmentBookDescriptionBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        bookDescriptionBinding = FragmentBookDescriptionBinding.inflate(layoutInflater)
+
+        return bookDescriptionBinding.root
+    }
 }
