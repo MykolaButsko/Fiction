ab8a8f9ecdc8a57b467b6ddfb45e337f26237801
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-14 10:05:21 +0300

    feat: add BookViewModelFactory for ViewModel creation

---
+++ app/src/main/java/com/example/fiction/viewmodel/BookViewModelFactory.kt
@@ -0,0 +1,15 @@
+package com.example.fiction.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class BookViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        val bookRepository = BookRepository(context)
+
+        return BookViewModel(bookRepository) as T
+    }
+}
