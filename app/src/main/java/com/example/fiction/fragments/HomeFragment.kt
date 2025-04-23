11a2401d22b7eedf360c413d1bdd400478f27b19
Mykola Butsko <mykolabutsko@gmail.com>
2025-04-23 10:39:05 +0300

    refactor: extend BaseFragment in HomeFragment

--- app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fiction/fragments/HomeFragment.kt
@@ -1,60 +1,32 @@
 package com.example.fiction.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.fiction.BookAdapter
-import com.example.fiction.BookDescriptionActivity
 import com.example.fiction.R
 import com.example.fiction.databinding.FragmentHomeBinding
-import com.example.fiction.viewmodel.BookViewModel
-import com.example.fiction.viewmodel.BookViewModelFactory
 
-class HomeFragment : Fragment() {
-
-    private lateinit var homeBinding: FragmentHomeBinding
-
-    private val bookViewModel: BookViewModel by viewModels {
-        BookViewModelFactory(requireContext())
-    }
-
-    private lateinit var bookAdapter: BookAdapter
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
+    FragmentHomeBinding::inflate
+) {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
-        homeBinding = FragmentHomeBinding.inflate(layoutInflater)
 
         init()
         chooseGenreBook()
 
-        return homeBinding.root
+        return binding.root
     }
 
     private fun init() {
 
-        bookAdapter = BookAdapter(
-
-            onOpenBookDescription = { book ->
-                val intent = Intent(requireContext(), BookDescriptionActivity::class.java).apply {
-                    putExtra("BOOK_NAME", book.bookName)
-                }
-                startActivity(intent)
-            },
-
-            onFavoriteToggle = { bookID ->
-                bookViewModel.isLiked(bookID)
-            }
-        )
-
-        homeBinding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
-        homeBinding.recyclerView.adapter = bookAdapter
+        binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.recyclerView.adapter = bookAdapter
 
         observeAndUpdateBooks()
 
@@ -67,7 +39,7 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun chooseGenreBook() = with(homeBinding) {
+    private fun chooseGenreBook() = with(binding) {
         buttonFiction.setOnClickListener {
             bookViewModel.loadBook()
             textViewFiction.text = (getString(R.string.app_name))
