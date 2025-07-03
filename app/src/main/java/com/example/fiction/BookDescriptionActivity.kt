6fe8b4685fa8ed87389b39de49221e1ee43ac4b4
Mykola Butsko <mykolabutsko@gmail.com>
2025-07-03 11:15:08 +0300

    feat: add intent factory method to BookDescriptionActivity

--- app/src/main/java/com/example/fiction/BookDescriptionActivity.kt
+++ app/src/main/java/com/example/fiction/BookDescriptionActivity.kt
@@ -1,11 +1,23 @@
 package com.example.fiction
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fiction.databinding.ActivityBookDescriptionBinding
 import com.example.fiction.fragments.BookDescriptionFragment
 
 class BookDescriptionActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_BOOK_NAME = "EXTRA_BOOK_NAME"
+
+        fun createIntent(context: Context, bookName: String): Intent {
+            return Intent(context, BookDescriptionActivity::class.java).apply {
+                putExtra(EXTRA_BOOK_NAME, bookName)
+            }
+        }
+    }
 
     private lateinit var activityBookDescription: ActivityBookDescriptionBinding
 
@@ -15,11 +27,11 @@ class BookDescriptionActivity : AppCompatActivity() {
         setContentView(activityBookDescription.root)
 
         if (savedInstanceState == null) {
-            val bookName = intent.getStringExtra("BOOK_NAME")
+            val bookName = intent.getStringExtra(EXTRA_BOOK_NAME)
 
             val bookDescriptionFragment = BookDescriptionFragment().apply {
                 arguments = Bundle().apply {
-                    putString("BOOK_NAME", bookName)
+                    putString(BookDescriptionFragment.ARG_BOOK_NAME, bookName)
                 }
             }
 
