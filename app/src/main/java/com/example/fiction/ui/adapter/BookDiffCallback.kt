09ad110a9a188834c77c742e4e4fa1863489fcb6
Mykola Butsko <mykolabutsko@gmail.com>
2025-12-15 16:12:19 +0200

    refactor: update BookDiffCallback after Book changes

--- app/src/main/java/com/example/fiction/ui/adapter/BookDiffCallback.kt
+++ app/src/main/java/com/example/fiction/ui/adapter/BookDiffCallback.kt
@@ -6,7 +6,7 @@ import com.example.fiction.data.model.Book
 class BookDiffCallback : DiffUtil.ItemCallback<Book>() {
 
     override fun areItemsTheSame(oldItem: Book, newItem: Book): Boolean {
-        return oldItem.bookID == newItem.bookID
+        return oldItem.bookId == newItem.bookId
     }
 
     override fun areContentsTheSame(oldItem: Book, newItem: Book): Boolean {
