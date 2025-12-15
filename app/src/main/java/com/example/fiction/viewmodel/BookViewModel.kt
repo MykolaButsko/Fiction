173cc6d72c0f6adefc06bc0739eb66e54201896d
Mykola Butsko <mykolabutsko@gmail.com>
2025-12-15 17:52:40 +0200

    refactor: update names and add onGenreSelected()

--- app/src/main/java/com/example/fiction/viewmodel/BookViewModel.kt
+++ app/src/main/java/com/example/fiction/viewmodel/BookViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.fiction.data.model.Book
+import com.example.fiction.data.model.Genre
 import com.example.fiction.data.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -16,8 +17,8 @@ class BookViewModel @Inject constructor(
     private val _bookList = MutableLiveData<List<Book>>()
     val bookListLiveData: LiveData<List<Book>> = _bookList
 
-    private val _favoriteListID = MutableLiveData<MutableSet<Int>>(mutableSetOf())
-    private val favoriteListID: LiveData<MutableSet<Int>> get() = _favoriteListID
+    private val _favoriteListId = MutableLiveData<MutableSet<Int>>(mutableSetOf())
+    private val favoriteListId: LiveData<MutableSet<Int>> get() = _favoriteListId
 
     private val _favListBooks = MutableLiveData<List<Book>>(emptyList())
     val favListBooks: LiveData<List<Book>> = _favListBooks
@@ -26,38 +27,46 @@ class BookViewModel @Inject constructor(
         _bookList.value = bookRepository.getBooks()
     }
 
-    fun filterBooksByGenre(genre: String) {
-        val filteredBooks = bookRepository.getBooksByGenre(genre)
+    private fun filterBookByGenre(genre: Genre) {
+        val filteredBooks = bookRepository.getBookByGenre(genre)
         _bookList.value = filteredBooks
     }
 
-    fun toggleFavorite(bookID: Int) {
-        val currentFavorite = _favoriteListID.value ?: mutableSetOf()
-        if (currentFavorite.contains(bookID)) {
-            currentFavorite.remove(bookID)
+    fun toggleFavorite(bookId: Int) {
+        val currentFavorite = _favoriteListId.value ?: mutableSetOf()
+        if (currentFavorite.contains(bookId)) {
+            currentFavorite.remove(bookId)
         } else {
-            currentFavorite.add(bookID)
+            currentFavorite.add(bookId)
         }
-        _favoriteListID.value = currentFavorite
-        updateFavoriteBookState(bookID)
-        filterFavBooks()
+        _favoriteListId.value = currentFavorite
+        updateFavoriteBookState(bookId)
+        filterFavBook()
     }
 
-    private fun updateFavoriteBookState(bookID: Int) {
+    private fun updateFavoriteBookState(id: Int) {
         val currentList = _bookList.value ?: return
-        val book = currentList.firstOrNull {it.bookID == bookID} ?: return
-        val newState = !book.toggleFavorite
+        val book = currentList.firstOrNull { it.bookId == id } ?: return
+        val newState = !book.isFavorite
 
-        bookRepository.updateFavoriteBook(bookID, newState)
+        bookRepository.updateFavoriteBook(id, newState)
 
         _bookList.value = bookRepository.getBooks()
     }
 
-    private fun filterFavBooks() {
+    private fun filterFavBook() {
         bookListLiveData.value?.let { allBooks ->
-            favoriteListID.value?.let { favoriteIDs ->
-                _favListBooks.value = allBooks.filter { it.bookID in favoriteIDs }
+            favoriteListId.value?.let { favoriteIds ->
+                _favListBooks.value = allBooks.filter { it.bookId in favoriteIds }
             }
+        }
+    }
+
+    fun onGenreSelected(genre: Genre) {
+        if (genre == Genre.FICTION) {
+            loadBook()
+        } else {
+            filterBookByGenre(genre)
         }
     }
 }
