422ca9b60f2ac942093f4a931e2b43e613d1742a
Mykola Butsko <mykolabutsko@gmail.com>
2025-12-22 15:09:52 +0200

    feat: add ALL and getTitle() in Genre enum

--- app/src/main/java/com/example/fiction/data/model/Genre.kt
+++ app/src/main/java/com/example/fiction/data/model/Genre.kt
@@ -1,16 +1,22 @@
 package com.example.fiction.data.model
 
+import android.content.Context
 import androidx.annotation.StringRes
 import com.example.fiction.R
 
 enum class Genre(
     val key: String,
-    @StringRes val titleRes: Int
+    @StringRes val titleRes: Int?
 ) {
+    ALL("all", null),
     FICTION("fiction", R.string.fiction),
     CULTURE("culture", R.string.culture_and_society),
     MIND("mind", R.string.mind_and_philosophy),
     GROWTH("growth", R.string.personal_growth);
+
+    fun getTitle(context: Context): String {
+        return titleRes?.let { context.getString(it) } ?: context.getString(R.string.all)
+    }
 
     companion object {
         fun fromKey(key: String?): Genre? =
